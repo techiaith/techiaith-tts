2c2f8fa2228f9cc59d521f7387f16d1b0cd6eead
Stephen Russell <str20tbl@bangor.ac.uk>
2023-05-17 12:56:14 +0100

    fixing imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ from datetime import datetime
 
 sys.path.insert(0, os.path.abspath("../../"))
 
-from techiaith import VERSION, VERSION_SHORT  # noqa: E402
+from techiaith.version import VERSION, VERSION_SHORT  # noqa: E402
 
 # -- Project information -----------------------------------------------------
 
