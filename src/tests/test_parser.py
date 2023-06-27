c71798e612edb40ba554353eb891904a97b2e199
Stephen Russell <str20tbl@bangor.ac.uk>
2023-06-27 06:35:54 +0100

    Chwilio am rhifau gyda NLP

---
+++ src/tests/test_parser.py
@@ -0,0 +1,82 @@
+from techiaith.tts.testun.parser import parse_text
+
+parse_text("""mae gan i 12 y cant o'r arian""")
+parse_text("""1af: 1 tbsp Olew olewydd oil
+ 2ail: 1 Winwnsyn mawr, heb groen ac wedi ei dorri yn ddeisiauiced
+ 3ydd: 2 Moronen, heb groen ac wedi eu torri yn ddeisiauunks
+ 4ydd: 2 Brigyn o seleri, heb y diweddion ac wedi eu torri yn ddeisiauiced
+ 5ed: 2 Clof garlleg, heb groen ac wedi eu torri’n fânpped
+ 6ed: 200 kg Cnau castan (defnyddiwch y rhai mewn paced os ydy’n haws), wedi eu hanerughly
+ 7fed: 275 g (Hanner jar) o domatos heulsych, gyda’r olew wedi eu draenio ac wedi eu torritorn
+ 8fed: 2 Afal, wedi eu creiddio a’u torru yn ddeisiauunks""")
+parse_text("""1: 1 tbsp Olew olewydd oil
+ 2: 1 Winwnsyn mawr, heb groen ac wedi ei dorri yn ddeisiauiced
+ 3: 2 Moronen, heb groen ac wedi eu torri yn ddeisiauunks
+ 4: 2 Brigyn o seleri, heb y diweddion ac wedi eu torri yn ddeisiauiced
+ 5: 2 Clof garlleg, heb groen ac wedi eu torri’n fânpped
+ 6: 200 kg Cnau castan (defnyddiwch y rhai mewn paced os ydy’n haws), wedi eu hanerughly
+ 7: 275 g (Hanner jar) o domatos heulsych, gyda’r olew wedi eu draenio ac wedi eu torritorn
+ 8: 2 Afal, wedi eu creiddio a’u torru yn ddeisiauunks""")
+parse_text("""Boneddigion a boneddigesau""")
+parse_text("""Annwyl Syr / Madam""")
+parse_text("""Yr eiddoch yn gywir""")
+parse_text("""Oddi wrth""")
+parse_text("""Hafan""")
+parse_text("""Amdanom ni""")
+parse_text("""Cysylltu â ni""")
+parse_text("""Cysylltwch â ni ar Twitter @S4C""")
+parse_text("""#yrawrgymraeg""")
+parse_text("""😉""")
+parse_text("""Anfonwch e-bost at: techiaith@bangor.ac.uk""")
+parse_text("""Grêt, mi drïai hynny felly.""")
+parse_text("""Cymraeg / Welsh""")
+parse_text("""Hwre!""")
+parse_text("""Haha!""")
+parse_text("""Da!""")
+parse_text("""Helo! Ti'n oce?""")
+parse_text("""Iawn?""")
+parse_text("""Shwmae?""")
+parse_text("""Sae moen e bennu nawr""")
+parse_text("""'Sdim ots pa bwysau y'f fi, ma'r struggle dal 'na.""")
+parse_text("""Paid""")
+parse_text("""Ar agor""")
+parse_text("""Safle ar gau""")
+parse_text("""Ffonia 999""")
+parse_text("""Wyt ti’n gwachad o?""")
+parse_text("""Ti'n oce?""")
+parse_text(
+    """Boneddigion a Boneddigesau, Bechgyn a Genethod, dowch yn llu! Mae atyniad newydd wedi cyrraedd Pen Y Pier... Y Syrcas! Ond nid syrcas arferol mo hon, o na!""")
+parse_text("""Mae'r llawr gwaelod (ground floor) wedi'i labelu'n Lefel 0.""")
+parse_text("""Mae bin olwynion du newydd yn costio £25.""")
+parse_text("""Treth Trafodiadau Tir: Dim treth ar brynu cartrefi am lai na £225,000.""")
+parse_text("""Y dyddiad yw'r 1af o Fawrth.""")
+parse_text(
+    """Bydd Cymru’n herio Bosnia-Herzegovina yn Stadiwm Dinas Caerdydd ddydd Iau (Hydref 6) – gyda’r gic gyntaf am 7:15 y nos.""")
+parse_text(
+    """“A fedrwn ni fod yn fwy gobeithiol na Saunders Lewis yn 1962? Yn sicr ni allwn fforddio anobeithio,” medd Dafydd Morgan Lewis, y golygydd.""")
+parse_text(
+    """Neithiwr, roedd y tymheredd yn 20.5 gradd selsiws yn Aberporth yng Ngheredigion, gan dorri’r record flaenorol o 18.9 gradd selsiws yn y Rhyl yn 1949.""")
+parse_text("""Bydd y trên nesaf sy'n cyrraedd platfform 2 yn gadael am Sanclêr am 4:35yp.""")
+parse_text("""Sefyllfa'r GIG 'ar ei waethaf' ers 25 mlynedd.""")
+parse_text("""Daw rhybudd undeb BMA Cymru wedi i weithiwr gwyno am brinder staff a morâl isel mewn un bwrdd iechyd.""")
+parse_text("""Gwynedd i ymgynghori ar dreth 300% ar ail gartrefi.""")
+parse_text(
+    """'Sa ti 'di deutha fi pan o'n i'n wyth mlwydd oed fyswn i yn y Pafiliwn, yn 34, yn gwisgo ffrog a sodla' o flaen cynulleidfa o 2,000  bobl, fyswn i ddim 'di dy goelio di.""")
+parse_text(
+    """Dywedodd Rebecca Evans, y Gweinidog Cyllid a Llywodraeth Leol: "Mae hwn yn newid sydd wedi ei deilwra i anghenion unigryw'r farchnad dai yng Nghymru ac mae'n cyfrannu at ein gweledigaeth ehangach o greu system drethi decach. "Ni fydd 61% o brynwyr tai yn gorfod talu treth. Bydd y newidiadau hyn yn rhoi cymorth i'r bobl sydd ei angen, ac yn helpu ag effaith y cynnydd mewn cyfraddau llog.""")
+parse_text(
+    """Yn ogystal, nid ydym yn argymell cymariaethau rhwng yr amcangyfrifon cysgu allan o'r casgliad misol hwn a'r cyfrif blynyddol o gysgu allan (hyd at fis Tachwedd 2019). Yn y casgliad misol hwn, gofynnir i awdurdodau lleol seilio eu hamcangyfrifon ar ddeallusrwydd lleol. Mae gan y cyfrif blynyddol o gysgu allan fethodoleg wahanol: ymarfer casglu gwybodaeth dwy wythnos, ac yna cyfrif ciplun un noson.""")
+parse_text(
+    """Nid i ni, O Arglwydd, nid i ni, Ond i’th enw dy hun, rho ogoniant, Er mwyn dy gariad a’th ffyddlondeb. Pam y mae’r cenhedloedd yn dweud, “Ble mae eu Duw?” Y mae ein Duw ni yn y nefoedd; Fe wna beth bynnag a ddymuna.""")
+parse_text(
+    """Mae'r Mesur hwn, a basiwyd gan Gynulliad Cenedlaethol Cymru ar 7 Rhagfyr 2010 ac a gymeradwywyd gan Ei Mawrhydi yn Ei Chyngor ar 9 Chwefror 2011, yn deddfu'r darpariaethau a ganlyn:""")
+parse_text("""Helo a chroeso i'r podlediad Am Blant.""")
+parse_text("""Oce, pnawn da pawb.""")
+parse_text(
+    """So ma'r concept 'ma o lunia' a rhannu llunia' a bo’ 'na lun yn gallu mynd yn fyd-eang mewn eiliada' o'i dynnu de a bod ch'mod ffôns 'fo ansawdd da a ballu mae o, mae o'n frawychus mewn ffor'.""")
+parse_text(
+    """A dwi'n meddwl hefyd fel rhieni bo' gyno ni rei arferion ansaff bob mis Medi ma' pawb efo ma' 'na lot does ar hyd y cyfrynga’ cymdeithasol llunia' a'r Facebook o blentyn yn mynd i'r ysgol yn ei wisg ysgol, mae'r bathodyn ysgol yna.""")
+parse_text(
+    """Wel un o'r petha' efo ecsbloetiaeth un o'r prif nodweddion ecsbloetiaeth ydi bo' bobol ddim yn dalld bo' n'w'n ca'l i ecsbloetio a ddim yn 'im yn gw'bod bo' 'na rywun yn cymud mantais ohonyn n'w ac yn dadla' bron iawn i'r gwrthwyneb ma'i cariad a parch a ballu sy'n mynd ymlaen mewn ffor'.""")
+parse_text(
+    """Be o'dd dy ymateb di i'r ffaith bod angen 'falla' i ni ail ystyried hawlia' plant yng ngoleuni'r COVID un deg naw?""")
