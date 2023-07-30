9138ca8d825f0772f64516cf573ccf9f16f277c7
Stephen Russell <str20tbl@bangor.ac.uk>
2023-07-30 16:26:35 +0100

    Fersiwn 0.2.0 gyda spacy fel parsiwr

--- setup.py
+++ setup.py
@@ -39,7 +39,10 @@ setup(
     packages=find_namespace_packages(where="src"),
     package_dir={"": "src"},
     package_data={"techiaith": ["py.typed"], "": ["*.txt"]},
-    install_requires=read_requirements("requirements.txt"),
+    install_requires=[
+        'pycocotools @ git+https://github.com/techiaith/spacy',
+        'pycocotools @ git+https://github.com/techiaith/spacy_cy_tag_lem_ner_lg/releases/download/23.03/cy_techiaith_tag_lem_ner_lg-0.0.1.tar.gz'
+    ],
     extras_require={"dev": read_requirements("dev-requirements.txt")},
     python_requires=">=3.8",
 )
